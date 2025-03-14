b8937d61b9921c5eac1c0585ebeefa8195a5c710
1flyti <yoran.croes24@gmail.com>
2025-03-14 15:29:29 +0100

    fixed everything with docker

--- app/database/connect.py
+++ app/database/connect.py
@@ -2,7 +2,10 @@ import os
 import psycopg2
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
+
+
 
 def connect_db():
     try:
@@ -10,14 +13,13 @@ def connect_db():
             dbname=os.getenv("POSTGRES_DB"),
             user=os.getenv("POSTGRES_USER"),
             password=os.getenv("POSTGRES_PASSWORD"),
-            host=os.getenv("DB_HOST", "localhost"),
-            port=os.getenv("DB_PORT", "5433")
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
         )
         print("Database connection established!")
         return connection
-
     except Exception as e:
-        print(f"Error: {e}")
+        print("Error connecting to the database:", e)
         return None
 
 
