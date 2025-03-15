fe73e665932b6c0659be46ea0e7b988f2dc96b40
1flyti <yoran.croes24@gmail.com>
2025-03-15 14:38:28 +0100

    fixed everything with docker

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "haaaa"
+    return "hoi"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
