582b86fdd15b33f73ec9b8f4c67b1577093e9ba6
1flyti <yoran.croes24@gmail.com>
2025-03-15 14:14:12 +0100

    fixed everything with docker

--- app/app.py
+++ app/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from database.init_db import init_db
+from app.database.init_db import init_db
 
 init_db()
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "haaaa"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
